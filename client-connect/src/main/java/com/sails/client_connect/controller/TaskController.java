fd3410c8263996800ceda742969c704c40cc907e
saiprajith2 <saiprajith2@gmail.com>
2024-08-21 18:52:53 +0530

    Added Tasks

--- client-connect/src/main/java/com/sails/client_connect/controller/TaskController.java
+++ client-connect/src/main/java/com/sails/client_connect/controller/TaskController.java
@@ -1,4 +1,11 @@
 package com.sails.client_connect.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
+
+
 }
